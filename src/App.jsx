ca43290c958440025ee6d4eca3c3732076fae8f0
Gustavo Torchelsen <gustavotorchelsen@gmail.com>
2023-06-08 17:19:48 -0300

    front

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './layout/Layout';
+import Home from './paginas/Home';
+import Produtos from './paginas/Produtos';
+import Vendas from './paginas/Vendas';
+import Vendedores from './paginas/Vendedores';
+
+
+function App() {
+  return (
+    <>
+      <Routes>
+          <Route path='/' element={<Layout><Home/></Layout>} />
+          <Route path='/produto' element={<Layout><Produtos/></Layout>} />
+          <Route path='/venda' element={<Layout><Vendas/></Layout>} />
+          <Route path='/vendedor' element={<Layout><Vendedores/></Layout>} />
+         
+
+      </Routes>     
+    </>
+  );
+}
+export default App;
